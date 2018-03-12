d9fce6e15811d617c252650dbd4e43450e9a9043
olorondu_emeka <oloronduchukwuemeka@gmail.com>
2018-03-12 19:47:37 +0100

    added javascriot file and updated app_style.scss

--- js/the_todo.js
+++ js/the_todo.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
-	
+	//variables
+	var serialNo = 0;
+	var addInput = $('#add-form input');
+	var addButton = $('#add-form button');
+	var taskBody = $('.task-body div');
+
+addButton.on("click", function(e){
+	e.preventDefault();
+//create new <ul> and <li> tags
+	var tasks = document.createElement('ul');
+
+	var firstLi = document.createElement('li');
+	var secondLi = document.createElement('li');
+	var thirdLi = document.createElement('li');
+
+	//append child elements
+	var secondElement = "<p>" + addInput.val() + "</p>";
+	secondElement += "<input type='checkbox'>";
+	var thirdElement = "<button><span class='fa fa-edit'></span> Edit</button><button><span class='fa fa-trash'></span> Delete</button>";
+	$(firstLi).text(serialNo);
+	$(secondLi).append(secondElement);
+	$(thirdLi).append(thirdElement);
+
+	tasks.append(firstLi);
+	tasks.append(secondLi);
+	tasks.append(thirdLi);
+	taskBody.append(tasks);
+
+
+	//add classes
+	$(tasks).addClass('task');
+
+});
 
 
 });
