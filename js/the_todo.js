132972b404b5ef74a40939d5fd3d5b50a3ddf945
olorondu_emeka <oloronduchukwuemeka@gmail.com>
2018-03-14 14:19:29 +0100

    updated delete button functionality and dynamic serial numbering

--- js/the_todo.js
+++ js/the_todo.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
 	//variables
-	var serialNo = 0;
 	var addInput = $('#add-form input');
 	var addButton = $('#add-form button');
 	var taskBody = $('.task-body div');
 
+//********Event for addButton**********
 addButton.on("click", function(e){
 	e.preventDefault();
 //create new <ul> and <li> tags
 	var tasks = document.createElement('ul');
-
 	var firstLi = document.createElement('li');
 	var secondLi = document.createElement('li');
 	var thirdLi = document.createElement('li');
@@ -18,7 +17,7 @@ addButton.on("click", function(e){
 	var secondElement = "<p>" + addInput.val() + "</p>";
 	secondElement += "<input type='checkbox'>";
 	var thirdElement = "<button><span class='fa fa-edit'></span> Edit</button><button><span class='fa fa-trash'></span> Delete</button>";
-	$(firstLi).text(serialNo);
+
 	$(secondLi).append(secondElement);
 	$(thirdLi).append(thirdElement);
 
@@ -27,11 +26,43 @@ addButton.on("click", function(e){
 	tasks.append(thirdLi);
 	taskBody.append(tasks);
 
-
 	//add classes
 	$(tasks).addClass('task');
+	$(thirdLi).addClass('action');
+	$(firstLi).addClass('serial-no');
+	addInput.val("");
+	var delButton = $('.action button:last-of-type');
+	delButton.addClass('delete');
 
+	//input serial numbers
+	var serialNo = $('.task').length;
+	$(firstLi).text(serialNo);
+
+
+});//end addButton event listener
+
+//********Event for delete button*******
+taskBody.on('click', function(e){
+	if(e.target.className == 'delete'){
+		var parent = $(e.target).parents('.task');
+		var nextIndex = parent.index();
+		var nextSibling = parent.next();
+		var tasks = $('.task');
+		var taskLength = $('.task').length;
+
+		//update serial numbers
+		for (var i = (nextIndex - 1); i < taskLength - 1; i++) {
+			$(tasks[i]).next().children('.serial-no').text(nextIndex);
+			nextIndex++;
+		}
+
+		//delete element
+		parent.remove();
+
+
+	}
 });
+
 
 
 });
