d216f3e85ce0ea62daaca4913a9cc520b36b7840
olorondu_emeka <oloronduchukwuemeka@gmail.com>
2018-03-18 01:02:40 +0100

    updated edit button functionality

--- js/the_todo.js
+++ js/the_todo.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var addInput = $('#add-form input');
 	var addButton = $('#add-form button');
 	var taskBody = $('.task-body div');
-
+	var buttonContainer = $('.task-body action')
 //********Event for addButton**********
 addButton.on("click", function(e){
 	e.preventDefault();
@@ -30,9 +30,12 @@ addButton.on("click", function(e){
 	$(tasks).addClass('task');
 	$(thirdLi).addClass('action');
 	$(firstLi).addClass('serial-no');
-	addInput.val("");
+	addInput.val("");  //clears addInput box
+
 	var delButton = $('.action button:last-of-type');
+	var editButton = $('.action button:first-of-type');
 	delButton.addClass('delete');
+	editButton.addClass('edit');
 
 	//input serial numbers
 	var serialNo = $('.task').length;
@@ -59,9 +62,44 @@ taskBody.on('click', function(e){
 		//delete element
 		parent.remove();
 
+	}
+
+	//Event handler for edit button
+	if (e.target.className == 'edit'){
+		var editBox = "<input type='text' style='padding: 1px'>";
+		var doneButton = "<button class='done'><span class='fa fa-check'></span> Done</button>";
+		var editLi = $(e.target).parents('.task').children('.serial-no').next();
+		var doneDesc = "<span class='fa fa-check'></span>";
+		var buttonSpan = $(e.target).children('span');
+
+		editLi.children('p').hide();
+		editLi.prepend(editBox);
+		$(e.target).parent().prepend(doneButton);
+		$(e.target).parent().children('.edit').remove();
+
 
 	}
-});
+
+//event handler for done button
+if (e.target.className == 'done'){
+console.log('doneee');
+	var editLi = $(e.target).parents('.task').children('.serial-no').next();
+	var editInput = editLi.children("input[type='text']");
+	var editValue = editInput.val();
+	var editButton = "<button class='edit'><span class='fa fa-edit'></span> Edit</button>"
+
+	editInput.hide();
+	editLi.children('p').show().text(editValue);
+	//remove done button and replace with edit button
+	$(e.target).parent().prepend(editButton);
+	$(e.target).parent().children('.done').remove();
+
+}
+
+});//end taskBody event handler
+
+
+
 
 
 
